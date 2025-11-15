75bd38e1f5ff7dcc8fa41cca2cbefa8b7ff22ff7
arthurd-d <arthurdahmerdz@gmail.com>
2025-11-15 14:31:14 -0300

    Ajustando bugs

---
+++ script.js
@@ -0,0 +1,11 @@
+const items = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+
+items.forEach(item => observer.observe(item));
